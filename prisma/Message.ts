e5c609f7654428ba5d50907da13de304066bdbef
Engineer <devop.jas@gmail.com>
2023-09-08 10:31:09 +0800

    added

--- prisma/Message.ts
+++ prisma/Message.ts
@@ -1,12 +1,18 @@
 import { prisma } from "../app/db";
 
 export async function GetMessage() {
-  const data = await prisma.guestbook.findMany({
-    take: 50,
-    orderBy: {
-      created_at: "desc",
-    },
-  });
+  try {
+    if (prisma) {
+      const data = await prisma.guestbook.findMany({
+        take: 50,
+        orderBy: {
+          created_at: "desc",
+        },
+      });
 
-  return data;
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
