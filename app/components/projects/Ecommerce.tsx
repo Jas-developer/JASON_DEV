23c847c2977e3c89e49e5c0ba41c41d93730d017
Jason <devop.jas@gmail.com>
2023-12-14 22:25:52 +0800

    added grids

--- app/components/projects/Ecommerce.tsx
+++ app/components/projects/Ecommerce.tsx
@@ -17,12 +17,16 @@ export default function Ecomm() {
       <form action="" className="grid grid-cols-3 gap-2">
         <div className="col-span-2 flex flex-col">
           <div className="flex flex-row">
-            <button>POST</button> <input type="text" />
+            <button>POST</button>{" "}
+            <input type="text" placeholder="type the domain and send" />
           </div>
           {/* json and terminal div */}
           <div className="flex flex-col">
-            <div className="grid grid-cols-2"></div>
-            <div></div>
+            <div className="grid flex-item grid-cols-2">
+              <div>fetch result-</div>
+              <div>request body</div>
+            </div>
+            <div className="flex-item"></div>
           </div>
         </div>
         <div className="col-span-1 flex flex-col">
@@ -31,7 +35,9 @@ export default function Ecomm() {
             <button>SEND</button>
           </div>
           {/* project information div */}
-          <div className="flex flex-col"></div>
+          <div className="flex flex-col">
+            <p>project information</p>
+          </div>
         </div>
       </form>
     </section>
