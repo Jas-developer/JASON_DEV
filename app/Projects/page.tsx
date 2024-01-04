853e3b117a0eebdc37d08e270ec312328150e1e7
Jason <devop.jas@gmail.com>
2024-01-04 10:13:56 +0800

    added note

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -1,7 +1,12 @@
 export default function Projects() {
   return (
     <div>
-      <h1>PROJECTS</h1>
+      <h1 className="text-2xl font-semibold text-green-500">PROJECTS</h1>
+      <br />
+      <span>
+        I PROVIDED A LINK FOR NOW IN THE HOME PAGE, THIS PORTFOLIO IS STILL
+        UNDER DEVELOPMENT.
+      </span>
     </div>
   );
 }
